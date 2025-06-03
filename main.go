4b14505dfd080f4c8cd9d43a2c49c0e729c7eaa2
runharu0662 <haruta141@icloud.com>
2025-06-03 22:18:29 +0900

    add_ip

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
     "fmt"
+    "net"
     "net/http"
 )
 
-func handlePost(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
-        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-        return
+func getLocalIP() string {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return "IP取得失敗"
     }
+    for _, addr := range addrs {
+        if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+            return ipnet.IP.String()
+        }
+    }
+    return "ローカルIPなし"
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) {
     name := r.FormValue("name")
     message := r.FormValue("message")
     fmt.Printf("投稿: %s - %s\n", name, message)
@@ -18,7 +28,10 @@ func handlePost(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/api/post", handlePost)
-    fmt.Println("Go API running on :8080")
+
+    localIP := getLocalIP()
+    fmt.Printf("Go API running on: http://%s:8080\n", localIP)
+
     http.ListenAndServe(":8080", nil)
 }
 
