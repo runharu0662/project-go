3c2fb42510aae5cdfcf4b06ec94a6c65e2f4f0c4
runharu0662 <haruta141@icloud.com>
2025-06-03 22:37:35 +0900

    try_fix_err

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "net/http"
 )
 
+// ローカルIPを取得する関数
 func getLocalIP() string {
     addrs, err := net.InterfaceAddrs()
     if err != nil {
@@ -19,7 +20,12 @@ func getLocalIP() string {
     return "ローカルIPなし"
 }
 
+// POSTを受け取って処理するハンドラ
 func handlePost(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+        return
+    }
     name := r.FormValue("name")
     message := r.FormValue("message")
     fmt.Printf("投稿: %s - %s\n", name, message)
@@ -32,6 +38,9 @@ func main() {
     localIP := getLocalIP()
     fmt.Printf("Go API running on: http://%s:8080\n", localIP)
 
-    http.ListenAndServe(":8080", nil)
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Println("サーバー起動失敗:", err)
+    }
 }
 
